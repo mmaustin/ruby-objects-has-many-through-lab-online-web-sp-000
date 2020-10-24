0245211b6d7e735800f3f8067217a5168a478009
mccray austin <tubach75@hotmail.com>
2020-10-24 14:55:37 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,26 @@
-c
+class Patient 
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select {|appoint| appoint.patient == self}
+  end
+
+  def doctors
+    appointments.map {|appoints| appoints.doctor}
+  end
+  
+end
