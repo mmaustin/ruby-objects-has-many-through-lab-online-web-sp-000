445eab98ff84eb2d21322ce9b214338f2afb0b53
mccray austin <tubach75@hotmail.com>
2020-10-24 13:57:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,8 +7,8 @@ class Artist
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
   def new_song(name, genre)
@@ -22,5 +22,5 @@ class Artist
   def genres
     songs.map {|song| song.genre}
   end
-  
+
 end
